d036476d47f63bc021e24403c22b3a65b6fb7e4c
Mateus Mayan <mateusmayan@icloud.com>
2025-03-23 14:07:51 -0300

    refactor ProductController and ProductService to support pagination in findAll method

--- src/main/java/com/mayan/ecommerce/controllers/ProductController.java
+++ src/main/java/com/mayan/ecommerce/controllers/ProductController.java
@@ -4,6 +4,8 @@ package com.mayan.ecommerce.controllers;
 import com.mayan.ecommerce.dtos.ProductDTO;
 import com.mayan.ecommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,8 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping
-    public List<ProductDTO> findAll(){
-        return service.findAll();
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 
     @GetMapping(value = "/{id}")
