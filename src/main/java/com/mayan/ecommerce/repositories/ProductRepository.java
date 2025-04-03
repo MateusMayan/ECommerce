95eea9b2fbd66f421f0fbed249775f400b387e5e
Mateus Mayan <mateusmayan@icloud.com>
2025-04-02 21:55:27 -0300

    add search functionality to findAll method in ProductController and ProductService

--- src/main/java/com/mayan/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/mayan/ecommerce/repositories/ProductRepository.java
@@ -1,7 +1,13 @@
 package com.mayan.ecommerce.repositories;
 
 import com.mayan.ecommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+        @Query("SELECT obj FROM Product obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+        Page<Product> searchByName(String name, Pageable pageable);
 }
