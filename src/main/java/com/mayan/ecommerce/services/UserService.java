90b71b02b4ab0362199821d8f3a4d3fe8cca047b
Mateus Mayan <mateusmayan@icloud.com>
2025-04-06 21:10:06 -0300

    add UserController, UserDTO, and update UserService for user retrieval

--- src/main/java/com/mayan/ecommerce/services/UserService.java
+++ src/main/java/com/mayan/ecommerce/services/UserService.java
@@ -1,16 +1,22 @@
 package com.mayan.ecommerce.services;
 
+import com.mayan.ecommerce.dtos.UserDTO;
 import com.mayan.ecommerce.entities.Role;
 import com.mayan.ecommerce.entities.User;
 import com.mayan.ecommerce.projections.UserDetailsProjection;
 import com.mayan.ecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -32,5 +38,23 @@ public class UserService implements UserDetailsService {
                 user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
             }
         return user;
+    }
+
+    protected User authenticated() {
+        try{
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+        String username = jwtPrincipal.getClaim("username");
+        return userRepository.findByEmail(username).isPresent() ? userRepository.findByEmail(username).get() : null;
+        }
+        catch (Exception e){
+            throw new UsernameNotFoundException("Email not found");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        User user = authenticated();
+        return new UserDTO(user);
     }
 }
