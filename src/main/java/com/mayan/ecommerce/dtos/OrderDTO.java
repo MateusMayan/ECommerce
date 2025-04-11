e107e200e8cc33630d7ffaa8908d353ba2d64c75
Mateus Mayan <mateusmayan@icloud.com>
2025-04-11 18:50:17 -0300

    add create order functionality with validation and item handling

--- src/main/java/com/mayan/ecommerce/dtos/OrderDTO.java
+++ src/main/java/com/mayan/ecommerce/dtos/OrderDTO.java
@@ -3,6 +3,7 @@ package com.mayan.ecommerce.dtos;
 import com.mayan.ecommerce.entities.Order;
 import com.mayan.ecommerce.entities.OrderItem;
 import com.mayan.ecommerce.entities.OrderStatus;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -14,6 +15,8 @@ public class OrderDTO {
     private OrderStatus status;
     private ClientDTO client;
     private PaymentDTO payment;
+
+    @NotEmpty(message =  "Deve ter pelo menos um item.")
     private List<OrderItemDTO> items = new ArrayList<>();
 
     public OrderDTO() {
--- src/main/java/com/mayan/ecommerce/services/OrderService.java
+++ src/main/java/com/mayan/ecommerce/services/OrderService.java
@@ -1,14 +1,18 @@
 package com.mayan.ecommerce.services;
 
 import com.mayan.ecommerce.dtos.OrderDTO;
+import com.mayan.ecommerce.dtos.OrderItemDTO;
 import com.mayan.ecommerce.dtos.ProductDTO;
-import com.mayan.ecommerce.entities.Order;
-import com.mayan.ecommerce.entities.Product;
+import com.mayan.ecommerce.entities.*;
+import com.mayan.ecommerce.repositories.OrderItemRepository;
 import com.mayan.ecommerce.repositories.OrderRepository;
+import com.mayan.ecommerce.repositories.ProductRepository;
 import com.mayan.ecommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
 
 @Service
 public class OrderService {
@@ -16,11 +20,39 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private UserService userService;
+
     @Transactional(readOnly = true)
     public OrderDTO findById(Long id) {
         Order result = repository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Pedido não encontrado.")
         );
         return new OrderDTO(result);
+    }
+
+    @Transactional
+    public OrderDTO create(OrderDTO dto) {
+        Order entity = new Order();
+        entity.setMoment(Instant.now());
+        entity.setStatus(OrderStatus.WAITING_PAYMENT);
+
+        User user = userService.authenticated();
+        entity.setClient(user);
+
+        for (OrderItemDTO orderItemDTO : dto.getItems()) {
+            Product product = productRepository.getReferenceById(orderItemDTO.getProductId());
+            OrderItem orderItem = new OrderItem(entity, product, orderItemDTO.getQuantity(), product.getPrice());
+            entity.getItems().add(orderItem);
+        }
+        entity = repository.save(entity);
+        orderItemRepository.saveAll(entity.getItems());
+        return new OrderDTO(entity);
     }
 }
